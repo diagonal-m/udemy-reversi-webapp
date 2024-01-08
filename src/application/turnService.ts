225394b47e162ce0bab8a2f66a1b425910d8a340
佐藤太一 <3101taichi@gmail.com>
2024-01-08 17:49:19 +0900

    gameRepository追加

--- src/application/turnService.ts
+++ src/application/turnService.ts
@@ -3,10 +3,12 @@ import { connectMySQL } from '../dataaccess/connection'
 import { toDisc } from '../domain/turn/disc'
 import { Point } from '../domain/turn/point'
 import { TurnRepository } from '../domain/turn/turnRepository'
+import { GameRepository } from '../domain/game/gameRepository'
 
 const gameGateway = new GameGateway()
 
 const turnRepository = new TurnRepository()
+const gameRepository = new GameRepository()
 
 class FindLatestGameTurnByTurnCountOutput {
   constructor(
@@ -37,12 +39,15 @@ export class TurnService {
   async findLatestGameTurnByTurnCount(turnCount: number): Promise<FindLatestGameTurnByTurnCountOutput> {
     const conn = await connectMySQL()
     try {
-      const gameRecord = await gameGateway.findLatest(conn)
-      if (!gameRecord) {
+      const game = await gameRepository.findLatest(conn)
+      if (!game) {
         throw new Error('Latest game not found')
       }
+      if (!game.id) {
+        throw new Error('game.id not exist')
+      }
 
-      const turn = await turnRepository.findForGameIdAndTurnCount(conn, gameRecord.id, turnCount)
+      const turn = await turnRepository.findForGameIdAndTurnCount(conn, game.id, turnCount)
 
       return new FindLatestGameTurnByTurnCountOutput(
         turnCount,
@@ -61,13 +66,16 @@ export class TurnService {
     const conn = await connectMySQL()
     try {
       // 1つ前のターンを取得する
-      const gameRecord = await gameGateway.findLatest(conn)
-      if (!gameRecord) {
+      const game = await gameRepository.findLatest(conn)
+      if (!game) {
         throw new Error('Latest game not found')
+      }
+      if (!game.id) {
+        throw new Error('game.id not exist')
       }
 
       const previoustTurnCount = turnCount - 1
-      const previousTurn = await turnRepository.findForGameIdAndTurnCount(conn, gameRecord.id, previoustTurnCount)
+      const previousTurn = await turnRepository.findForGameIdAndTurnCount(conn, game.id, previoustTurnCount)
 
       // 石を置く
       const newTurn = previousTurn.placeNext(toDisc(disc), new Point(x, y))

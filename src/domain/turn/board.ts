22b91e0c209c688334f6661f3c19035ab4e2ec94
佐藤太一 <3101taichi@gmail.com>
2024-01-08 22:42:17 +0900

    75

--- src/domain/turn/board.ts
+++ src/domain/turn/board.ts
@@ -1,11 +1,25 @@
 import { Disc } from "./disc";
 import { Move } from "./move";
+import { Point } from "./point";
 
 export class Board {
   constructor(private _discs: Disc[][]) {}
 
   place(move: Move): Board {
     // TODO 盤面におけるかチェック
+
+    // 空のマス目ではない場合、置くことはできない
+    if (this._discs[move.point.y][move.point.x] !== Disc.Empty) {
+      throw new Error('Selected point is not empty')
+    }
+
+    // ひっくり返せる点をリストアップ
+    const flipPoints = this.listFlipPoints()
+
+    // ひっくり返せる点がない場合、置くことはできない
+    if (flipPoints.length === 0) {
+      throw new Error('Flip points is empty')
+    }
 
     // 盤面をコピー
     const newDiscs = this._discs.map((line) => {
@@ -21,6 +35,10 @@ export class Board {
 
     return new Board(newDiscs)
   }
+
+  private listFlipPoints(): Point[] {
+    return [new Point(0, 0)]
+  } 
 
   get discs() {
     return this._discs
